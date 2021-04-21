852843a14bf7ef9219bed5f9ce9ab0e9ebaa57d8
LilRookie69 <ridhoiyoy962013@gmail.com>
2021-04-21 11:53:14 +0700

    add navtabs to transaction

--- src/views/Transaction.vue
+++ src/views/Transaction.vue
@@ -1,51 +1,144 @@
 <template>
   <dashboard-layouts>
-    <div class="flex flex-col lg:flex-row justify-center p-3  flex-grow h-full">
-      <div class="w-full p-5 bg-white rounded-xl">
-        <t-button class="mb-2" @click="onCreateOrder">
-          New Order
-        </t-button>
-        <t-table :headers="headers" :data="orderList.data">
-          <template slot="row" slot-scope="props">
-            <tr
-              @click="onSelectRow(props.row)"
-              :class="[
-                props.trClass,
-                // props.rowIndex % 2 === 0 ? 'bg-gray-100' : ''
-              ]"
+    <div class="flex flex-wrap">
+      <div class="w-full">
+        <ul class="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row">
+          <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+            <a
+              class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal"
+              v-on:click="toggleTabs(1)"
+              v-bind:class="{
+                'text-blue-500 bg-white': openTab !== 1,
+                'text-white bg-blue-500': openTab === 1,
+              }"
             >
-              <td :class="props.tdClass">
-                {{ props.row.id }}
-              </td>
-              <td :class="props.tdClass">
-                {{ props.row.order_number }}
-              </td>
-              <td :class="props.tdClass">
-                {{ props.row.order_code }}
-              </td>
-              <td :class="props.tdClass">
-                {{ props.row.created_at }}
-              </td>
-              <td :class="props.tdClass">
-                <button
-                  v-if="props.row.status == 1"
-                  class="block px-4 py-1 transition duration-100 ease-in-out focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed bg-yellow-300"
-                >
-                  Pending
-                </button>
-                <button
-                  v-else-if="props.row.status == 2"
-                  class="block px-4 py-1 transition duration-100 ease-in-out focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed bg-green-300"
-                >
-                  Success
-                </button>
-              </td>
-              <td :class="props.tdClass">
-                {{ props.row.total_price }}
-              </td>
-            </tr>
-          </template>
-        </t-table>
+              Order List
+            </a>
+          </li>
+          <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+            <a
+              class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal"
+              v-on:click="toggleTabs(2)"
+              v-bind:class="{
+                'text-blue-500 bg-white': openTab !== 2,
+                'text-white bg-blue-500': openTab === 2,
+              }"
+            >
+              History
+            </a>
+          </li>
+        </ul>
+        <div
+          class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded"
+        >
+          <div class="px-4 py-5 flex-auto">
+            <div class="tab-content tab-space">
+              <div
+                v-bind:class="{
+                  hidden: openTab !== 1,
+                  block: openTab === 1,
+                }"
+              >
+                <div>
+                  <t-button class="mb-2" @click="onCreateOrder">
+                    New Order
+                  </t-button>
+                  <t-table :headers="headers" :data="orderList.data">
+                    <template slot="row" slot-scope="props">
+                      <tr
+                        @click="onSelectRow(props.row)"
+                        :class="[
+                          props.trClass,
+                          // props.rowIndex % 2 === 0 ? 'bg-gray-100' : ''
+                        ]"
+                      >
+                        <td :class="props.tdClass">
+                          {{ props.row.id }}
+                        </td>
+                        <td :class="props.tdClass">
+                          {{ props.row.order_number }}
+                        </td>
+                        <td :class="props.tdClass">
+                          {{ props.row.order_code }}
+                        </td>
+                        <td :class="props.tdClass">
+                          {{ props.row.created_at }}
+                        </td>
+                        <td :class="props.tdClass">
+                          <button
+                            v-if="props.row.status == 1"
+                            class="block px-4 py-1 transition duration-100 ease-in-out focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed bg-yellow-300"
+                          >
+                            Pending
+                          </button>
+                          <button
+                            v-else-if="props.row.status == 2"
+                            class="block px-4 py-1 transition duration-100 ease-in-out focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed bg-green-300"
+                          >
+                            Success
+                          </button>
+                        </td>
+                        <td :class="props.tdClass">
+                          {{ props.row.total_price }}
+                        </td>
+                      </tr>
+                    </template>
+                  </t-table>
+                </div>
+              </div>
+              <div
+                v-bind:class="{
+                  hidden: openTab !== 2,
+                  block: openTab === 2,
+                }"
+              >
+                <div>
+                  <t-table :headers="headers" :data="orderList.data">
+                    <template slot="row" slot-scope="props">
+                      <tr
+                        @click="onSelectRow(props.row)"
+                        :class="[
+                          props.trClass,
+                          // props.rowIndex % 2 === 0 ? 'bg-gray-100' : ''
+                        ]"
+                      >
+                        <td :class="props.tdClass">
+                          {{ props.row.id }}
+                        </td>
+                        <td :class="props.tdClass">
+                          {{ props.row.order_number }}
+                        </td>
+                        <td :class="props.tdClass">
+                          {{ props.row.order_code }}
+                        </td>
+                        <td :class="props.tdClass">
+                          {{ props.row.created_at }}
+                        </td>
+                        <td :class="props.tdClass">
+                          <button
+                            v-if="props.row.status == 1"
+                            class="block px-4 py-1 transition duration-100 ease-in-out focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed bg-yellow-300"
+                          >
+                            Pending
+                          </button>
+                          <button
+                            v-else-if="props.row.status == 2"
+                            class="block px-4 py-1 transition duration-100 ease-in-out focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed bg-green-300"
+                          >
+                            Success
+                          </button>
+                        </td>
+                        <td :class="props.tdClass">
+                          {{ props.row.total_price }}
+                        </td>
+                      </tr>
+                    </template>
+                  </t-table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </dashboard-layouts>
@@ -57,6 +150,7 @@ export default {
   components: { DashboardLayouts },
   data() {
     return {
+      openTab: 1,
       headers: [
         {
           value: "id",
@@ -94,10 +188,13 @@ export default {
   methods: {
     ...mapActions("order", {
       getAllOrderList: "getAllOrderList",
-      createOrder: "createOrder",
     }),
+    createOrder: "createOrder",
     fetchData() {
       this.getAllOrderList();
+    },
+    toggleTabs: function(tabNumber) {
+      this.openTab = tabNumber;
     },
     async onCreateOrder() {
       await this.createOrder();
